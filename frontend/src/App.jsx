c2df5028d46231e343900d60c1a8d5483587b818
Aditya Agarwal <agarwal.aditya393@gmail.com>
2025-08-15 21:37:01 +0530

    setting up router-dom routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import toast, {Toaster} from 'react-hot-toast';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import SignupPage from './pages/Signuppage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const notifu = () => toast('here is your toast',{
-    duration:1000
-  });
 
   return (
     <div>
-      <button onClick={notifu}>onclick</button>
-      <Toaster/>
+      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/Signup' element={<SignupPage/>}/>        
+        <Route path='/login' element={<LoginPage/>}/>        
+        <Route path='/settings' element={<SettingsPage/>}/>        
+        <Route path='/profile' element={<ProfilePage/>}/>        
+      </Routes>
+
     </div>
   )
 }
